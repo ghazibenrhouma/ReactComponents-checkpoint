d1213bed9673b991865fb84bb6ed61884c3eddd5
ghazibenrhouma <ghazybenrhouma@gmail.com>
2020-09-11 13:51:15 +0100

    first commit

---
+++ recat-components/src/profile/Address.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Address(){
+    const styleAddress = {
+        textAlign: "center",
+        fontFamily: "cursive",
+        letterSpacing: 3
+    }
+    return <p style={styleAddress}>Route Téniour CHIHIA</p>
+}
+export default Address;
---
+++ recat-components/src/profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export function FullName(){
+    const styleName = {
+        fontStyle: "italic",
+        fontFamily: "fantasy",
+        textAlign: "center",
+        color: "pink",
+        letterSpacing: 10
+    }
+    return <h1 style={styleName}>Ghazi BEN RHOUMA</h1>
+}
